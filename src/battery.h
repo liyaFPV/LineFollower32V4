ac8dc77c3d50300163103005584f04ab3d76c9ff
liyaFPV <lbkmzc942@gmail.com>
2026-05-23 22:11:22 +0500

    не значительные изменеине

--- src/battery.h
+++ src/battery.h
@@ -3,9 +3,9 @@
 #include "config.h"
 
 // Напряжения батареи (в мВ)
-#define BATTERY_CRITICAL_MV 1500  // Критическое напряжение (полностью разряжена)
-#define BATTERY_LOW_MV 1600        // Низкое напряжение (нужно зарядить)
-#define BATTERY_FULL_MV 2100       // Полная батарея
+#define BATTERY_CRITICAL_MV 3000  // Критическое напряжение (полностью разряжена)
+#define BATTERY_LOW_MV 3200        // Низкое напряжение (нужно зарядить)
+#define BATTERY_FULL_MV 4200       // Полная батарея
 
 extern float dividerTop;
 extern float dividerBottom;
